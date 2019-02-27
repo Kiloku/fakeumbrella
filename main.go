77ae67ed48056528673d64ea96fa7a5fc6b96a1f
Mateus Lacerda <mateus.sateles@gmail.com>
2019-02-27 18:40:21 -0300

    Gets the weather data based on user ID

--- main.go
+++ main.go
@@ -1,15 +1,68 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	
+	"encoding/json"
+	
+	"io/ioutil"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var db *gorm.DB
 var err error
+
+
+//Generated with "JSON-to-Go" tool. (https://mholt.github.io/json-to-go/) and modified by hand to remove unnecessary elements
+type WeatherData struct {
+	Message float64 `json:"message"`
+	List    []struct {
+		Dt   int `json:"dt"`
+		Main struct {
+			Temp      float64 `json:"temp"`
+			TempMin   float64 `json:"temp_min"`
+			TempMax   float64 `json:"temp_max"`
+			Pressure  float64 `json:"pressure"`
+			SeaLevel  float64 `json:"sea_level"`
+			GrndLevel float64 `json:"grnd_level"`
+			Humidity  int     `json:"humidity"`
+			TempKf    int     `json:"temp_kf"`
+		} `json:"main"`
+		Weather []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+			Deg   float64 `json:"deg"`
+		} `json:"wind"`
+		Sys struct {
+			Pod string `json:"pod"`
+		} `json:"sys"`
+		DtTxt string `json:"dt_txt"`
+		Snow  struct {
+			ThreeH float64 `json:"3h"`
+		} `json:"snow,omitempty"`
+	} `json:"list"`
+	City struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Coord struct {
+			Lat float64 `json:"lat"`
+			Lon float64 `json:"lon"`
+		} `json:"coord"`
+		Country string `json:"country"`
+	} `json:"city"`
+}
 
 type Customer struct {
 	ID uint `json:"id"`
@@ -32,6 +85,7 @@ func main(){
   r := gin.Default()
   r.GET("/customers/", GetCustomers)
   r.GET("/customers/:id", GetCustomer)
+  r.GET("/weather/:id", GetWeatherCustomer)
   r.POST("/customers", CreateCustomer)
   r.PUT("/customers/:id", UpdateCustomer)
   r.DELETE("/customers/:id", DeleteCustomer)
@@ -40,42 +94,42 @@ func main(){
 
 func DeleteCustomer(c *gin.Context) {
 	id := c.Params.ByName("id")
-	var Customer Customer
-	d := db.Where("id = ?", id).Delete(&Customer)
+	var customer Customer
+	d := db.Where("id = ?", id).Delete(&customer)
 	fmt.Println(d)
 	c.JSON(200, gin.H{"id #" + id: "deleted"})
 }
 
 func UpdateCustomer(c *gin.Context) {
-	var Customer Customer
+	var customer Customer
 	id := c.Params.ByName("id")
 
-	if err := db.Where("id = ?", id).First(&Customer).Error; err != nil {
+	if err := db.Where("id = ?", id).First(&customer).Error; err != nil {
 		c.AbortWithStatus(404)
 		fmt.Println(err)
 	}
-	c.BindJSON(&Customer)
+	c.BindJSON(&customer)
 
-	db.Save(&Customer)
-	c.JSON(200, Customer)
+	db.Save(&customer)
+	c.JSON(200, customer)
 }
 
 func CreateCustomer(c *gin.Context) {
-	var Customer Customer
-	c.BindJSON(&Customer)
+	var customer Customer
+	c.BindJSON(&customer)
 
-	db.Create(&Customer)
-	c.JSON(200, Customer)
+	db.Create(&customer)
+	c.JSON(200, customer)
 }
 
 func GetCustomer(c *gin.Context) {
 	id := c.Params.ByName("id")
-	var Customer Customer
-	if err := db.Where("id = ?", id).First(&Customer).Error; err != nil {
+	var customer Customer
+	if err := db.Where("id = ?", id).First(&customer).Error; err != nil {
 		c.AbortWithStatus(404)
 		fmt.Println(err)
 	} else {
-		c.JSON(200, Customer)
+		c.JSON(200, customer)
 	}
 }
 
@@ -88,4 +142,33 @@ func GetCustomers(c *gin.Context) {
  	} else {
     	c.JSON(200, customers)
 	}
+}
+
+func GetWeatherCustomer(c *gin.Context) { 
+	id := c.Params.ByName("id")
+	var customer Customer
+	if err := db.Where("id = ?", id).First(&customer).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		fmt.Println(customer.Location)
+		c.JSON(200, RequestWeatherAt(customer.Location))//gin.H{"location": customer.Location})//customer.Location)
+		//c.JSON(200, Customer)
+	}
+}
+
+func RequestWeatherAt(location string) (weatherData WeatherData){
+	response, err := http.Get("http://api.openweathermap.org/data/2.5/forecast?q="+location+"&APPID=2aa5d8c417225481239400cc3a8a5409")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with err %s", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data)) //Remove
+		json.Unmarshal(data, &weatherData)	
+		
+	}
+	//weatherData := WeatherData{}
+	return 
+	
+	//jsonData := map[string]string
 }
