b5ac090e20934877de201eef3268f5a629f1e890
Mateus Lacerda <mateus.sateles@gmail.com>
2019-03-03 10:00:38 -0300

    Simple CSS to look slightly nice

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -31,6 +31,22 @@ export class HomeComponent implements OnInit {
 	getForecasts(){
         this.http.get("http://localhost:8080/forecasts/customers/").subscribe((res : any)=>{
             this.forecasts = res;
+            /*this.forecasts.sort((a, b) => {
+            	if(a.Forecast.willRain && b.Forecast.willRain) return 0;
+            	if(a.Forecast.willRain && !b.Forecast.willRain) return 1;
+            	if(!a.Forecast.willRain && b.Forecast.willRain) return 1;
+            })
+            this.forecasts.reverse();*/
+            for (let i = 0; i < this.forecasts.length; i++)
+            {
+            	if (!this.forecasts[i].Forecast.willRain)
+            	{
+            		console.log("splicing");
+            		let temp = this.forecasts[i];
+            		this.forecasts.splice(i, 1)
+            		this.forecasts.push(temp);
+            	}
+            }
             console.log(this.forecasts);
 			this.chart.render(this.customers, this.forecasts);
         });
